8df6511b9830310e0031ab97f9793bccfb23e0cb
Ricardo <rgsilva@users.noreply.github.com>
2023-09-02 13:56:02 -0300

    Revert "Skipping report if already reported recently"
    This reverts commit 101102f3ecb507f4cb1d571f224672d680e18220.

--- src/main.py
+++ src/main.py
@@ -13,7 +13,7 @@ from config import load_config
 def _mac_to_id(mac: str) -> str:
     return mac.lower().replace("-", "")
 
-def run(last_present: list[str] = [], last_away: list[str] = []):
+def run():
     config = load_config("config.json")
 
     omada = Omada(
@@ -42,39 +42,28 @@ def run(last_present: list[str] = [], last_away: list[str] = []):
         clients = omada.clients(site["id"])
         for client in clients:
             lsstr = datetime.datetime.fromtimestamp(client["lastSeen"]/1000).strftime("%Y-%m-%d %H:%M:%S")
-            mac_address = client["mac"]
-            print(mac_address, client["name"], lsstr, end=" ")
+            print(client["mac"], client["name"], lsstr, end=" ")
 
-            if mac_address not in config.devices:
+            if client["mac"] not in config.devices:
                 print("[unknown]")
                 continue
-            present.append(mac_address)
-            print("[known]")
+            present.append(client["mac"])
+
+            ha.set_state(_mac_to_id(client["mac"]), "home")
+            print("[updated]")
 
             # NOTE: hack to make it work properly.
             time.sleep(0.01)
 
-    for mac in present:
-        if mac in last_present:
-            print("Already reported at home", mac)
-            continue
-        print("Reporting at home", mac)
-        ha.set_state(_mac_to_id(mac), "home")
-
     missing = list(set(config.devices.keys()) - set(present))
     for mac in missing:
-        if mac in last_away:
-            print("Already reported away", mac)
-            continue
-        print("Reporting away", mac)
+        print("Missing", mac)
         ha.set_state(_mac_to_id(mac), "not_home")
 
     omada.logout()
     ha.disconnect()
 
     time.sleep(config.interval)
-
-    return present, missing
 
 def terminate(signal, frame):
     print("Terminating...")
@@ -83,9 +72,7 @@ def terminate(signal, frame):
 if __name__ == "__main__":
     signal.signal(signal.SIGTERM, terminate)
 
-    last_present = []
-    last_missing = []
     while True:
         print("-----", datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "-----")
-        last_present, last_missing = run(last_present, last_missing)
+        run()
         print()
